4fa46bd5b693f77e4336e4a955709c4d12425819
jcanell4 <jcanell4@xtec.cat>
2024-03-02 16:00:16 +0100

    fix errors

--- build/lib/py_portada_image/__init__.py
+++ build/lib/py_portada_image/__init__.py
@@ -1,2 +1,2 @@
 __version__ = "0.1.1"
-from .deskew_tools import deskew_tool, deskewSkimage, isSkimageSkewed, deskewImageFile
+from .deskew_tools import DeskewTool, deskewSkimage, isSkimageSkewed, deskewImageFile
