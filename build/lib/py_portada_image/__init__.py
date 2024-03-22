c615a4cb88ef51be0f102a8eb1a062c3a2957c28
jcanell4 <jcanell4@xtec.cat>
2024-03-23 00:42:57 +0100

    version 0.1.6

--- build/lib/py_portada_image/__init__.py
+++
@@ -1,2 +0,0 @@
-__version__ = "0.1.1"
-from .deskew_tools import DeskewTool, deskewSkimage, isSkimageSkewed, deskewImageFile
--- setup.py
+++ setup.py
@@ -1,11 +1,17 @@
 from setuptools import setup
+from pathlib import Path
+
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
 
 setup(name='py_portada_image',
-    version='0.1.5',
+    version='0.1.6',
     description='tools for processing images within the PortADa project',
     author='PortADa team',
     author_email='jcbportada@gmail.com',
     license='MIT',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
     url="https://github.com/portada-git/py_portada_image",
     packages=['py_portada_image'],
     py_modules=['deskew_tools','dewarp_tools'],
